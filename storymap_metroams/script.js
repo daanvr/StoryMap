b6a41ab607dba30b6c655ca74bf11211483bf68e
Daan van Rasmhorst <daanvr@gmail.com>
2018-12-05 12:24:55 +0100

    Update storymap_metroams/script.js

--- storymap_metroams/script.js
+++ storymap_metroams/script.js
@@ -106,7 +106,7 @@ chapters[5] = {
     LegendaToggles: [["mra-weg-int", "mra-weg-int-lz"], ["mra-weg-ext", "mra-weg-ext-lz"], ["mra-weg-door", "mra-weg-door-lz"]],
     Popuptext: ["Intern", "Extern", "Doorgaand"],
     PopupData: ["auto_int", "auto_ex", "auto_door"],
-    popuprounded: true,
+    popuprounded: [true, true, true],
     NbrRoundedBij: 1000,
     Variables: ["auto_int", "auto_ex", "auto_door"],
     StyleDivider: 3000,
@@ -133,7 +133,7 @@ chapters[6] = {
     LegendaToggles: [["mra-ov-int", "mra-ov-int-lz"], ["mra-ov-ext", "mra-ov-ext-lz"], ["mra-ov-door", "mra-ov-door-lz"]],
     Popuptext: ["Intern", "Extern", "Doorgaand"],
     PopupData: ["ov_int", "ov_ex", "ov_door"],
-    popuprounded: true,
+    popuprounded: [true, true, true],
     NbrRoundedBij: 1000,
     StyleDivider: 3000,
     PopupPercentage: true,
@@ -169,7 +169,7 @@ chapters[9] = {
     LegendaToggles: ["spider_verpl_OV", "spider_verpl_auto"],
     Popuptext: ["OV", "Auto"],
     PopupData: ["TOT_OV", "TOT_auto"],
-    popuprounded: true,
+    popuprounded: [true, true],
     NbrRoundedBij: 100,
     PopupPercentage: true,
     PopupDataUnit: [undefined],
@@ -192,7 +192,7 @@ chapters[10] = {
     LegendaToggles: ["spider_verpl_woonwerk", "spider_verpl_onderwijs", "spider_verpl_zakelijk"],
     Popuptext: ["Woon-Werk", "Onderwijs", "Zakelijk"],
     PopupData: ["WW_alles", "OND_alles", "ZK_alles"],
-    popuprounded: true,
+    popuprounded: [true, true, true],
     NbrRoundedBij: 100,
     StyleDivider: 10000,
     PopupPercentage: true,
@@ -215,7 +215,7 @@ chapters[11] = {
     //LegendaToggles: ["spider_verpl_15-", "spider_verpl_15-39", "spider_verpl_40-65", "spider_verpl_65plus"],
     Popuptext: ["15 de 39", "40 tot 65", "65 plus"],
     PopupData: ["LFT_15_39", "LFT_40_65", "LFT_65"],
-    popuprounded: true,
+    popuprounded: [true, true, true],
     NbrRoundedBij: 100,
     StyleDivider: 10000,
     PopupPercentage: true,
@@ -252,7 +252,7 @@ chapters[13] = {
     LegendaToggles: ["", "", ""],
     Popuptext: ["Auto", "Openbaar Vervoer", "Fiets en Lopen"],
     PopupData: ["TOT_auto", "TOT_OV", "TOT_fiets"],
-    popuprounded: true,
+    popuprounded: [true, true, true],
     NbrRoundedBij: 1000,
     PopupPercentage: true,
     PopupDataUnit: [undefined],
@@ -291,6 +291,7 @@ chapters[15] = {
     PopupDataUnit: ["", "%", "%"],
     popupNothingAfterComma: [false, true, true],
     PopupPercentage: false,
+    popuprounded: [false, false, false],
     StyleDivider: "",
     InfoIcon: false,
     iconsrc: "imgs/trafficlight.png",
@@ -312,6 +313,7 @@ chapters[16] = {
     PopupDataUnit: ["", "%", "%"],
     popupNothingAfterComma: [false, true, true],
     PopupPercentage: false,
+    popuprounded: [false, false, false],
     StyleDivider: "",
     InfoIcon: false,
     iconsrc: "imgs/trafficlight.png",
@@ -333,6 +335,7 @@ chapters[17] = {
     PopupDataUnit: ["", "%", "%"],
     popupNothingAfterComma: [false, true, true],
     PopupPercentage: false,
+    popuprounded: [false, false, false],
     StyleDivider: "",
     InfoIcon: true,
     InfoText: "data: CBS, Banen van werknemers, 2006-2016",
@@ -369,6 +372,8 @@ chapters[19] = {
     PopupDataUnit: [""," inwoners", " inwoners"],
     popupNothingAfterComma: false,
     PopupPercentage: false,
+    popuprounded: [false, true, true],
+    NbrRoundedBij: 1000,
     StyleDivider: "",
     InfoIcon: true,
     InfoText: "data: CBS, Banen van werknemers, 2006-2016",
@@ -392,6 +397,7 @@ chapters[20] = {
     PopupDataUnit: [""," duizend banen", " duizend banen"],
     popupNothingAfterComma: false,
     PopupPercentage: false,
+    popuprounded: [false, false, false],
     StyleDivider: "",
     InfoIcon: true,
     InfoText: "data: CBS, Banen van werknemers, 2006-2016",
@@ -676,7 +682,7 @@ function LayerFilter(Chap) {
                     //fill the popoup content cue 
                     PopupHtmlContent += chapters[chapnbrpp].Popuptext[i];
                     PopupHtmlContent += ": ";
-                    if (chapters[chapnbrpp].popuprounded === true) {//if rounded true then round
+                    if (chapters[chapnbrpp].popuprounded[i] === true) {//if rounded true then round
                         RoundedNumber = HoverdData.properties[chapters[chapnbrpp].PopupData[i]]; //load not rounded data
                         RoundedNumber = chapters[chapnbrpp].NbrRoundedBij * Math.ceil(RoundedNumber / chapters[chapnbrpp].NbrRoundedBij); //round bij the number defined in chapter
                         PopupHtmlContent += RoundedNumber; //set rounded number in popup content
